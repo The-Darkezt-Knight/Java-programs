376698b87008c15c72c0d63e72e1bf45d54a1dbf
The-Darkezt-Knight <babap1104@gmail.com>
2025-10-04 21:05:41 +0800

    Add files via upload

---
+++ First_Occurrence.java
@@ -0,0 +1,13 @@
+class First_Occurence
+{
+    public int strStr(String haystack, String needle) {
+        int result = (haystack.contains(needle))? haystack.indexOf(needle) :  -1;
+        return result;
+    }
+    public static void main(String[] args) {
+        String s1 = "leetcode";
+        String s2 = "leeto";
+        First_Occurence obj = new First_Occurence();
+        System.out.println(obj.strStr(s1, s2));
+    }
+}
